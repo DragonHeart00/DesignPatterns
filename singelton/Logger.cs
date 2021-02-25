f352745a4ef97d99fd855317e68ee7478ade8f09
kasassulaiman <s195462@student.dtu.dk>
2021-02-25 14:33:07 +0100

    Add project files.

---
+++ singelton/Logger.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace DesignPatterns
+{
+    class Logger
+    {
+
+
+        private static Logger logger;
+        
+        //set constructor as a private 
+        private Logger()
+        {
+
+        }
+
+        public static Logger GetInstance()
+        {
+            if(logger == null)
+            {
+                logger = new Logger();
+            }
+            return logger;
+        }
+    }
+}
---
+++ singelton/TestLogger.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DesignPatterns.singelton
+{
+    class TestLogger
+    {
+        static void Main(string[] args)
+        {
+           
+
+            Console.WriteLine("-----Singelton-----");
+            Logger obj1 = Logger.GetInstance();
+            Logger obj2 = Logger.GetInstance();
+            Console.WriteLine(obj1.GetHashCode());
+            Console.WriteLine(obj2.GetHashCode());
+
+            Console.WriteLine("-----Non Singelton-----");
+            Test obj3 = new Test();
+            Test obj4 = new Test();
+            Console.WriteLine(obj3.GetHashCode());
+            Console.WriteLine(obj4.GetHashCode());
+
+        }
+    }
+
+
+    class Test
+    {
+
+    }
+}
+
+/* Only create one instance of a class.
+ * if we want a class to hav only one object, then we use the singleton pattern 
+ */
+
+/*
+ * we need only one object of a class.
+ * objects used for logging, device drivers for printers or registry setting need 
+ * to have only one of a kind. if there are multipe obejcts we may encounter problems like 
+ * incorrect program behavior, inconsistent results etc. 
+ */
